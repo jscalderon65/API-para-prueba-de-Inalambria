57410bf68406a7bf936da8100d7551a568161d83
Sebastian Calderon <jhonnseca0599@gmail.com>
2021-07-17 16:48:29 -0500

    Procfile

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ const IndexRoutes = require("./Routes/index.routes.js");
 const cors = require('cors');
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+app.set("port", process.env.PORT || 3000);
 app.use(IndexRoutes);
 module.exports = app;
